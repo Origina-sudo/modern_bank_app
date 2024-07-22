3752f90c846efb6e3c167c587790cba744778a3f
Kelvin <kelvinaf122@gmail.com>
2024-07-22 14:40:09 +0000

    Structure

--- bank_app/src/App.jsx
+++ bank_app/src/App.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
+import styles from './style';
 
-const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+const App = () =>(
+    <div className='bg-primary w-full overflow-hidden'>
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          NavBar
+        </div>
+      </div>
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}> 
+            Hero
+        </div>
+      </div>
+      <div className={`bg-primary ${styles.paddingX}${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}> 
+            Stats
+            Business
+            Billing 
+            CardDeal 
+            TTestimonial    
+            Clients
+            CTA
+            Footer            </div>
+      </div>
+
+    </div>
+  );
+
 
 export default App
